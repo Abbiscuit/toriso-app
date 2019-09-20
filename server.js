56f072cdc6bace27c57820c0fe7ae94c59cfa642
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-09-20 16:41:59 +0900

    Build users route template

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // Routes
-app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
