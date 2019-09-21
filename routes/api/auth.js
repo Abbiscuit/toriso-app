e4c97520a7a304087bc123dff408b958636b389f
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-09-21 13:54:15 +0900

    Add auth & user model and route #1

---
+++ routes/api/auth.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+// @route        GET api/AUTH
+// @desc         Auth Route Test
+// @access       Public
+
+router.get('/', (req, res) => {
+  res.json({ msg: 'Auth route' });
+});
+
+// @route        POST api/AUTH
+// @desc         Auth Route Test
+// @access       Public
+
+router.post('/', (req, res) => {
+  res.json({ msg: 'Login user & get JWT token' });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 
 // Routes
 app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
