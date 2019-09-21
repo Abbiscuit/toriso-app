36e54f56ff3e688f75b35f3dda2b2ebd196f4b24
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-09-21 17:53:46 +0900

    Build auth middleware to check auth #1

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json({ extended: false }));
 
 // Routes
 app.use('/api/users', require('./routes/api/users'));
-// app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
