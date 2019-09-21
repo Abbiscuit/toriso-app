1bd8fa12a0789ff8d6cb481f106d531e3d7269e9
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-09-21 17:23:52 +0900

    Connecte mongodb to the application #1

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-// @route        GET api/AUTH
-// @desc         Auth Route Test
-// @access       Public
+// // @route        GET api/AUTH
+// // @desc         Auth Route Test
+// // @access       Public
 
-router.get('/', (req, res) => {
-  res.json({ msg: 'Auth route' });
-});
+// router.get('/', (req, res) => {
+//   res.json({ msg: 'Auth route' });
+// });
 
-// @route        POST api/AUTH
-// @desc         Auth Route Test
-// @access       Public
+// // @route        POST api/AUTH
+// // @desc         Auth Route Test
+// // @access       Public
 
-router.post('/', (req, res) => {
-  res.json({ msg: 'Login user & get JWT token' });
-});
+// router.post('/', (req, res) => {
+//   res.json({ msg: 'Login user & get JWT token' });
+// });
 
-module.exports = router;
+// module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+
+// ConnectDB
+connectDB();
+
+// Middleware
+app.use(express.json({ extended: false }));
 
 // Routes
 app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
